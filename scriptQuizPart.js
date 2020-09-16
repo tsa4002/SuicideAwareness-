5c6e059ccbc08c0a964d05f4dce5ace19d4d78e2
Andrew Springer <andrewspringer@andrews-macbook.wireless.davidson.edu>
2020-09-16 18:39:40 -0400

    created a new quiz using the HTML form tag

---
+++ scriptQuizPart.js
@@ -0,0 +1,29 @@
+function submitAnswer(){
+    if(document.getElementById("two").checked){
+        alert("correct!");
+    }else{
+        alert("false!");
+    }
+    //y = document.getElementById("two").value;
+    //z = document.getElementById("three").value;
+
+}
+
+function submitAnswerTwo(){
+    if(document.getElementById("four").checked){
+        alert("correct!");
+    }else{
+        alert("false!");
+    }
+    //y = document.getElementById("two").value;
+    //z = document.getElementById("three").value;
+
+}
+
+function submitAnswerThree(){
+    if(document.getElementById("six").checked){
+        alert("correct!");
+    }else{
+        alert("false!");
+    }
+}
